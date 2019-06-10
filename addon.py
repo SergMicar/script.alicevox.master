c34ba3aaee80d3b3efc6459ce258f587cd1b630a
Sergey Goncharov <34092984+SergMicar@users.noreply.github.com>
2019-06-10 16:40:21 +0300

    Add files via upload

---
+++ addon.py
@@ -0,0 +1,159 @@
+import os
+import sys
+import time
+import urlparse
+import xbmcaddon
+import xbmcgui
+import xbmc
+import pyxbmct
+import time
+import json
+import urllib
+import urllib2
+
+__addon__      = xbmcaddon.Addon()
+__cwd__        = xbmc.translatePath( __addon__.getAddonInfo('path') ).decode("utf-8")
+
+url = 'http://192.168.1.2/post_service'
+opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
+data = urllib.urlencode({'event' : 'test',
+                         'time'  : '10'})
+
+
+
+#xbmc.translatePath(path)#'special://masterprofile/script_data' -> '/home/user/XBMC/UserData/script_data' on Linux
+
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+addon_dir = addon.getAddonInfo('path')
+
+#logo_attention = os.path.join(addon_dir, 'resources', 'img', "attention.png")
+#logo_construction = os.path.join(addon_dir, 'resources', 'img', "construction.png")
+logo_mdm = os.path.join(addon_dir, 'resources', 'img', "mdm.png") #worked only one/last instanse of os.path.join()
+
+SOUND_WELCOME = os.path.join(addon_dir, 'resources', 'media', "welcome.wav")
+SOUND_RINGTONE = os.path.join(addon_dir, 'resources', 'media', "ringtone.wav")
+SOUND_INCALL = os.path.join(addon_dir, 'resources', 'media', "incall.wav")
+SOUND_CALLEND = os.path.join(addon_dir, 'resources', 'media', "callend.wav")
+SOUND_BATLOW = os.path.join(addon_dir, 'resources', 'media', "batlow.wav")
+SOUND_SINCALL = os.path.join(addon_dir, 'resources', 'media', "Sincall.wav")
+SOUND_EINCALL = os.path.join(addon_dir, 'resources', 'media', "Eincall.wav")
+SOUND_SBATLOW = os.path.join(addon_dir, 'resources', 'media', "Sbatlow.wav")
+SOUND_EBATLOW = os.path.join(addon_dir, 'resources', 'media', "Ebatlow.wav")
+
+dialog = xbmcgui.Dialog()
+
+def isPlaybackPaused():
+    return bool(xbmc.getCondVisibility("Player.Paused"))
+
+def isMuted():
+    return bool(xbmc.getCondVisibility("Player.Muted"))
+
+def play():
+	if isPlaybackPaused(): xbmc.Player().pause() #trigger
+    
+
+def pause():
+	if not isPlaybackPaused(): xbmc.Player().pause() #trigger
+    
+	
+	
+
+#	xbmcgui.Dialog().ok(addonname, str(isPlaybackPaused()))
+#	xbmcgui.Dialog().ok(addonname, str(isMuted()))
+
+
+
+#--MAIN--
+if len(sys.argv) == 2:
+	xbmc.enableNavSounds(True)
+	xbmc.stopSFX()
+	
+	if sys.argv[1] != "MESSAGE":
+		if "/cms/cached/voice/" in sys.argv[1]:
+			play()
+			xbmc.executebuiltin('XBMC.Notification(TTS arrived, trying to say it...)')
+			xbmc.playSFX(sys.argv[1])
+		elif sys.argv[1] == "welcome":
+			play()
+			xbmc.executebuiltin('XBMC.Notification(Built-in phrase called, "WELCOME")')
+			xbmc.playSFX(SOUND_WELCOME)
+		elif sys.argv[1] == "ringtone":
+			play()
+			xbmc.executebuiltin('XBMC.Notification(Built-in sound called, "RINGTONE")')
+			xbmc.playSFX(SOUND_RINGTONE)
+		elif sys.argv[1] == "incall":
+			play()
+			xbmc.executebuiltin('XBMC.Notification(Built-in phrase called, "INCOMING CALL")')
+			xbmc.playSFX(SOUND_INCALL)
+		elif sys.argv[1] == "callend":
+			play()
+			xbmc.executebuiltin('XBMC.Notification(Built-in phrase called, "CALL RELEASE")')
+			xbmc.playSFX(SOUND_CALLEND)
+		elif sys.argv[1] == "batlow":
+			play()
+			xbmc.executebuiltin('XBMC.Notification(Built-in phrase called, "BATTERY PHONE IS LOW")')
+			xbmc.playSFX(SOUND_BATLOW)
+		elif sys.argv[1] == "Sincall":
+			play()
+			xbmc.playSFX(SOUND_SINCALL)
+		elif sys.argv[1] == "Eincall":
+			play()
+			xbmc.playSFX(SOUND_EINCALL)
+		elif sys.argv[1] == "Sbatlow":
+			play()
+			xbmc.playSFX(SOUND_SBATLOW)
+		elif sys.argv[1] == "Ebatlow":
+			play()
+			xbmc.playSFX(SOUND_EBATLOW)
+		elif sys.argv[1] == "STOP": xbmc.stopSFX()
+		else: xbmc.executebuiltin('XBMC.Notification('+addonname+': ERROR, command '+sys.argv[1]+' not recognized)')
+elif len(sys.argv) > 2 and sys.argv[1] == "MESSAGE":
+	window = pyxbmct.AddonDialogWindow(sys.argv[2]) #-Create a window instance
+	window.setGeometry(650, 170, 1, 5) #-Set the window width, height and the grid resolution: 2 rows, 3 columns
+	if len(sys.argv) == 4:
+		showtime = 5
+		url=logo_mdm
+	elif len(sys.argv) == 5:
+		showtime = float(sys.argv[4])
+		url=logo_mdm
+	elif len(sys.argv) == 6:
+		showtime = float(sys.argv[4])
+		url=logo_mdm if sys.argv[5] == "mdm" else sys.argv[5]
+	image = pyxbmct.Image(url) #-Create a logo
+	message = pyxbmct.Label(sys.argv[3], alignment=pyxbmct.ALIGN_CENTER, font="34") #-Create a text label, argument font="27" is not working :(
+#	message = pyxbmct.TextBox(sys.argv[3])
+#	message.setAnimations([('WindowOpen', 'effect=fade start=0 end=100 time=2000',), ('WindowClose', 'effect=fade start=100 end=0 time=2000',)])
+	window.placeControl(image, 0, 0) #-Place the img on the window grid
+	window.placeControl(message, 0, 1, rowspan=1, columnspan=4) #-Place the label/textbox on the window grid
+	window.show()
+	time.sleep(showtime)
+	window.close()
+else:
+	xbmc.executebuiltin('XBMC.Notification('+addonname+': ERROR, have not correct data payload)')
+
+
+
+#xbmc.LOGDEBUG = 0
+#xbmc.LOGINFO = 1
+#xbmc.LOGNOTICE = 2
+#xbmc.LOGWARNING = 3
+#xbmc.LOGERROR = 4
+#xbmc.LOGSEVERE = 5
+#xbmc.LOGFATAL = 6
+#xbmc.LOGNONE = 7
+try: 
+	response = opener.open(url, data=data).read() #perform operations
+except urllib2.HTTPError, e:
+	xbmc.log('HTTPError = ' + str(e.code), 3)
+	dialog.ok(addonname+': HTTPError', 'Failed to connect '+url, str(e.code))
+except urllib2.URLError, e:
+	xbmc.log('URLError = ' + str(e.reason), 3)
+	dialog.ok(addonname+': URLError', 'Failed to connect', str(e.reason))
+except httplib.HTTPException, e:
+	xbmc.log('HTTPException', 3)
+	dialog.ok(addonname+': HTTPException', 'Failed connect to SmartHome', url)
+except Exception:
+	import traceback
+	xbmc.log('generic exception: ' + str(traceback.format_exc()), 3)
+
---
+++ readme.txt
@@ -0,0 +1,35 @@
+Примеры вызова:
+
+Вывод звука из файла на локальном диске:
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["D:\\ringtone.wav"]},"id":1}
+
+Вывод звука по http ссылке:
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["http://192.168.1.2/cms/cached/voice/aebd42dddcca11fa8b8d5ad4d75793d3_google.wav"]},"id":1}
+
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["http://192.168.1.2/cms/cached/voice/rh_e4768dae4160a3eb9a57713580eff5e6.wav"]},"id":1}
+
+Вывод стандартного звука плагина:
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["welcome"]},"id":1}
+
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["ringtone"]},"id":1}
+
+Возможные значения: welcome, ringtone, incall, callend, batlow, Sincall, Eincall, Sbatlow, Ebatlow, STOP
+при этом при передаче параметра STOP - остановить текущее воспроизведение
+
+Вывод сообщения:
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["MESSAGE", "SmartHome Alice", "Проверка подключения"]},"id":1}
+
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["MESSAGE", "SmartHome Alice", "Проверка подключения", 7]},"id":1}
+
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["MESSAGE", "SmartHome Alice", "Проверка подключения", "7", "mdm"]},"id":1}
+
+http://xbmc:xbmc@192.168.1.51:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.alicevox.master","params":["MESSAGE", "SmartHome Alice", "Проверка подключения", "7", "http://192.168.1.2/img/logo_small.png"]},"id":1}
+
+где:
+7=длительность показа сообщения в секундах
+mdm=стандартная картинка, вместо "mdm" может быть url ссылка на файл, например "http://192.168.1.2/img/logo.png"
+ 
+где:
+xbmc:xbmc - логин и пароль к KODI
+192.168.1.51 - KODI
+192.168.1.2 - удаленный сервер с хостингом картинок (например MDM)
