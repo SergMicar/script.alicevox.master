08ec52a90facb2fb91c45c41f0d2d9b4e1d3688e
SergMicar <Sergey.V.Goncharov@gmail.com>
2021-10-28 15:26:52 +0300

    extend debug

--- addon.py
+++ addon.py
@@ -59,7 +59,8 @@ message_color = settings.getSetting("message_color")         #str
 debug = settings.getSetting("debug")                         #bool->str
 debug = "true"
 
-if debug == "true": xbmc.log('ALICEVOX CONFIG -> tts_notification='+tts_notification+', notification_time='+notification_time+', tts_stop='+tts_stop+', media_unpause='+media_unpause+', message_type='+message_type+', message_size='+message_size+', message_color='+message_color, 2)
+if debug == "true": xbmc.log('ALICEVOX CONFIG -> tts_notification(on_message_arrived)='+tts_notification+', notification_time='+notification_time+', tts_stop='+tts_stop+', media_unpause='+media_unpause, 2)
+if debug == "true": xbmc.log('ALICEVOX CONFIG -> message_type='+message_type+', message_size='+message_size+', message_color='+message_color, 2)
 
 #0xTTRRGGBB where T is the transparency value, R is red, G is green and as you guessed B is blue
 colors = ['0xFFFF0000', '0xFFFFD700', '0xFF00FF00', '0xFF0000FF','0xFF8000FF']
