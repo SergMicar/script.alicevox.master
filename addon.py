78aca5a86a2fa1044d0d36623a309fcbf2967b89
SergMicar <Sergey.V.Goncharov@gmail.com>
2021-11-01 16:33:11 +0200

    add: xbmc.log('ALICEVOX call, enable debug in plugin settings if you need more information', 2)

--- addon.py
+++ addon.py
@@ -60,6 +60,7 @@ message_size = settings.getSetting("message_size")           #str
 message_color = settings.getSetting("message_color")         #str
 debug = settings.getSetting("debug")                         #bool->str
 
+xbmc.log('ALICEVOX call, enable debug in plugin settings if you need more information', 2)
 if debug == "true": xbmc.log('ALICEVOX CONFIG -> tts_notification(on_message_arrived)='+tts_notification+', notification_time='+notification_time+', tts_stop='+tts_stop+', media_unpause='+media_unpause, 2)
 if debug == "true": xbmc.log('ALICEVOX CONFIG -> message_type='+message_type+', message_size='+message_size+', message_color='+message_color, 2)
 
