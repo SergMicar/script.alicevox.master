97dccfde514499f299a024da3238b62aa7d4136e
Sergey V. Goncharov <Sergey.V.Goncharov@gmail.com>
2021-11-05 15:13:50 +0200

    extend debug message

--- addon.py
+++ addon.py
@@ -62,18 +62,18 @@ debug = settings.getSetting("debug")                         #bool->str
 
 if debug == "false": xbmc.log('ALICEVOX call, enable debug in plugin settings if you need more information', 2)
 if debug == "true":
-    m_type = "Unknown"
+    m_type = "Unknown="+message_type
     if message_type == "0": m_type = "classic"
     if message_type == "1": m_type = "xbmcgui"
     if message_type == "2": m_type = "pyxbmct"
-    m_color = "Unknown"
+    m_color = "Unknown="+message_color
     if message_color == "0": m_color = "red"
     if message_color == "1": m_color = "yellow"
     if message_color == "2": m_color = "green"
     if message_color == "3": m_color = "blue"
     if message_color == "4": m_color = "purple"
-    xbmc.log('ALICEVOX CONFIG -> classic_message_on_tts_arrived='+tts_notification+', notification_time='+notification_time+', tts_stop='+tts_stop+', media_unpause='+media_unpause, 2)
-    xbmc.log('ALICEVOX CONFIG -> message_type='+m_type+', message_size='+message_size+', message_color='+m_color, 2)
+    xbmc.log('ALICEVOX CONFIG -> classic_message_on_tts='+tts_notification+', notification_time='+notification_time+', tts_stop='+tts_stop+', media_unpause='+media_unpause, 2)
+    xbmc.log('ALICEVOX CONFIG -> message_type='+m_type+'('+message_type+'), message_size='+message_size+', message_color='+m_color+'('+message_color+')', 2)
 
 #0xTTRRGGBB where T is the transparency value, R is red, G is green and as you guessed B is blue
 colors = ['0xFFFF0000', '0xFFFFD700', '0xFF00FF00', '0xFF0000FF','0xFF8000FF']
